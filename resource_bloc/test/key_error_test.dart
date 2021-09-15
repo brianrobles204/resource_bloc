1f202307d1d2815dd967339b98f3b4b658a00611
Brian Robles <brianrobles204@gmail.com>
2021-09-16 00:43:52 +0800

    Added tests for key error events

--- resource_bloc/test/key_error_test.dart
+++ resource_bloc/test/key_error_test.dart
@@ -1,3 +1,4 @@
+import 'package:resource_bloc/src/resource_event.dart';
 import 'package:test/test.dart';
 
 import 'test_utils.dart';
@@ -14,18 +15,161 @@ void main() {
       bloc.close();
     });
 
-    test('pass errors to the state', () {});
+    test('can happen during bloc initialization', () async {
+      expectLater(
+        bloc.stream,
+        emitsInOrder(<dynamic>[
+          isKeyErrorState,
+          emitsDone,
+        ]),
+      );
 
-    test('overwrite existing data with an error', () {});
+      bloc.add(KeyError(StateError('error')));
+      await pumpEventQueue();
 
-    test('after errors are still reflected', () {});
+      bloc.close();
+    });
 
-    test('stop all loading including further fresh / truth updates', () {});
+    test('overwrite existing data with an error', () async {
+      expectLater(
+        bloc.stream,
+        emitsInOrder(<dynamic>[
+          isInitialLoadingState('key'),
+          isStateWith(isLoading: false, content: 'a', error: isNull),
+          isStateWhere(isLoading: false, value: isNull, error: isStateError),
+        ]),
+      );
 
-    test('stop all loading even during reload', () {});
+      bloc.freshContent = 'a';
+      bloc.key = 'key';
+      await untilDone(bloc);
 
-    test('stop all loading even during truth read', () {});
+      bloc.add(KeyError(StateError('error')));
+    });
 
-    test('stop all loading even during truth write', () {});
+    test('after errors are still reflected', () async {
+      expectLater(
+        bloc.stream,
+        emitsInOrder(<dynamic>[
+          isInitialLoadingState('key'),
+          isStateWhere(
+              isLoading: false, value: isNull, error: isFormatException),
+          isStateWhere(isLoading: false, value: isNull, error: isStateError),
+        ]),
+      );
+
+      bloc.freshValueThrowable = FormatException();
+      bloc.key = 'key';
+      await untilDone(bloc);
+
+      bloc.add(KeyError(StateError('error')));
+    });
+
+    test('stop all loading including further fresh / truth updates', () async {
+      expectLater(
+        bloc.stream,
+        emitsInOrder(<dynamic>[
+          isInitialLoadingState('key'),
+          isStateWith(isLoading: false, content: 'key-x', error: isNull),
+          isKeyErrorState,
+          emitsDone,
+        ]),
+      );
+
+      final freshSink = bloc.applyStreamFreshSource();
+      bloc.key = 'key';
+      await pumpEventQueue();
+
+      freshSink.add((key) => '$key-x');
+      await pumpEventQueue();
+
+      bloc.add(KeyError(StateError('error')));
+      await pumpEventQueue();
+
+      // Update fresh and truth source after key error, should do nothing
+      freshSink.add((key) => '$key-y');
+      await pumpEventQueue();
+
+      bloc.getTruthSource('key').value =
+          bloc.createFreshValue('key', content: 'truth');
+      await pumpEventQueue();
+
+      freshSink.addError(FormatException());
+      await pumpEventQueue();
+
+      bloc.getTruthSource('key').addError(Exception());
+      await pumpEventQueue();
+
+      bloc.close();
+    });
+
+    test('stop all loading even during reload', () async {
+      expectLater(
+        bloc.stream,
+        emitsInOrder(<dynamic>[
+          isInitialLoadingState('key'),
+          isKeyErrorState,
+          emitsDone,
+        ]),
+      );
+
+      bloc.freshValueLocked.value = true;
+      bloc.key = 'key';
+      await pumpEventQueue();
+
+      bloc.add(KeyError(StateError('error')));
+      await pumpEventQueue();
+
+      bloc.freshValueLocked.value = false;
+      await pumpEventQueue();
+
+      bloc.close();
+    });
+
+    test('stop all loading even during truth read', () async {
+      expectLater(
+        bloc.stream,
+        emitsInOrder(<dynamic>[
+          isInitialLoadingState('key'),
+          isKeyErrorState,
+          emitsDone,
+        ]),
+      );
+
+      bloc.truthReadLocked.value = true;
+      bloc.key = 'key';
+      await pumpEventQueue();
+
+      bloc.add(KeyError(StateError('error')));
+      await pumpEventQueue();
+
+      bloc.truthReadLocked.value = false;
+      await pumpEventQueue();
+
+      bloc.close();
+    });
+
+    test('stop all loading even during truth write', () async {
+      expectLater(
+        bloc.stream,
+        emitsInOrder(<dynamic>[
+          isInitialLoadingState('key'),
+          isKeyErrorState,
+          emitsDone,
+        ]),
+      );
+
+      bloc.truthWriteLocked.value = true;
+      bloc.key = 'key';
+      await pumpEventQueue();
+
+      bloc.add(KeyError(StateError('error')));
+      await pumpEventQueue();
+
+      bloc.truthWriteLocked.value = false;
+      await pumpEventQueue();
+
+      bloc.close();
+    });
   });
 }
