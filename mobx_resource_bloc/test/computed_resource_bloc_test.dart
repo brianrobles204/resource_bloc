e6564277ddb36a582ef01ec0ec28c0cf50f203e2
Brian Robles <brianrobles204@gmail.com>
2021-09-24 13:12:03 +0800

    Test how resource bloc behaves when (un)observed
    Should be able to subscribe, cancel, and later resubscribe to stream. Any changes in key should not trigger a key update.

--- mobx_resource_bloc/test/computed_resource_bloc_test.dart
+++ mobx_resource_bloc/test/computed_resource_bloc_test.dart
@@ -367,5 +367,60 @@ void main() {
         ]),
       );
     });
+
+    test('stream can be listened, cancelled, and listened again', () async {
+      final states = <ResourceState<String, String>>[];
+
+      final sub1 = bloc.stream.listen(states.add);
+      await pumpEventQueue();
+      sub1.cancel();
+
+      final sub2 = bloc.stream.listen(states.add);
+      await pumpEventQueue();
+      sub2.cancel();
+
+      expect(
+        states,
+        equals([
+          ResourceState<String, String>.initial('key', isLoading: true),
+          ResourceState<String, String>.withValue('key', 'key',
+              isLoading: false, source: Source.fresh),
+          ResourceState<String, String>.withValue('key', 'key',
+              isLoading: true, source: Source.fresh),
+          ResourceState<String, String>.withValue('key', 'key',
+              isLoading: false, source: Source.fresh),
+        ]),
+      );
+    });
+
+    test('key change while unobserved will not start load', () async {
+      final states = <ResourceState<String, String>>[];
+
+      final sub1 = bloc.stream.listen(states.add);
+      await pumpEventQueue();
+      sub1.cancel();
+
+      updateObs(key, 'second');
+      await pumpEventQueue();
+
+      final sub2 = bloc.stream.listen(states.add);
+      await pumpEventQueue();
+      sub2.cancel();
+
+      expect(keyCount, equals(3));
+      expect(freshCount, equals(2));
+
+      expect(
+        states,
+        equals([
+          ResourceState<String, String>.initial('key', isLoading: true),
+          ResourceState<String, String>.withValue('key', 'key',
+              isLoading: false, source: Source.fresh),
+          ResourceState<String, String>.initial('second', isLoading: true),
+          ResourceState<String, String>.withValue('second', 'second',
+              isLoading: false, source: Source.fresh),
+        ]),
+      );
+    });
   });
 }
